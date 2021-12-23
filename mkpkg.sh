15551decf65528c021dbe21888b1986ba5ae753c
Yonle <yonle@protonmail.com>
2021-12-23 21:22:21 +0700

    Fixbug

--- mkpkg.sh
+++ mkpkg.sh
@@ -1,22 +1,28 @@
 #!/usr/bin/env bash
 
 if [ $# = 0 ]; then
-	echo "Usage: mkpkg <command> <target>"
+	echo "Usage: mkpkg <commands>"
 else
 	cwd=`pwd`
-	mkdir -p $cwd/.tmp/$1
-	cd $cwd/.tmp/$1
+	for cmd in $@; do
+		! command -v $cmd > /dev/null && echo "$cmd not found" &&  exit 1
+		[ -d $cwd/.tmp/$cmd ] && rm -rf $cwd/.tmp/$cmd
+		mkdir -p $cwd/.tmp/$cmd
+		cd $cwd/.tmp/$cmd
 
-	for i in lib lib64 bin; do
-		[ -d $PREFIX/$i ] && mkdir $i
+		for i in lib lib64 bin; do
+			[ -d $PREFIX/$i ] && mkdir $i
+		done
+
+		for lib in `ldd $(command -v $cmd)`; do
+			if [ "$lib" != "libc.so" ]; then
+				[ -e $PREFIX/lib64/$lib ] && cp $PREFIX/lib64/$lib lib64
+				[ -e $PREFIX/lib/$lib ] && cp $PREFIX/lib/$lib lib
+			fi
+		done
+
+		cp $(command -v $cmd) bin
+		tar -czf $cwd/$cmd.tgz .
+		[ $? = 0 ] && rm -rf $cwd/.tmp/$cmd
 	done
-
-	for lib in `ldd $(command -v $1)`; do
-		[ -e $PREFIX/lib64/$lib ] && cp $PREFIX/lib64/$lib lib64
-		[ -e $PREFIX/lib/$lib ] && cp $PREFIX/lib/$lib lib
-	done
-
-	cp $(command -v $1) bin
-	tar -cWzf $cwd/${2:-$1.tgz} .
-	[ $? = 0 ] && rm -rf $cwd/.tmp/$1
 fi
